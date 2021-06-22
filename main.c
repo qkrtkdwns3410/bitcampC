bf9c445c158ff17418fb6f1dacea287520424ee9
psj <qkrtkdwns3410@naver.com>
2021-06-22 22:39:25 +0900

    6-22

--- main.c
+++ main.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;
-
-    if (a > 0) {
-        if (a >= 100) {
-            printf("Great");
-        } else {
-            printf("Good");
-        }
-    } else if (a < 0) {
-        printf("Negative");
-    } else {
-        printf("Zero");
-    }
-
-    return 0;
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d %d", a, b);
 }
