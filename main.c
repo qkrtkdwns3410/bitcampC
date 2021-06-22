19c87b340229697e45f5766d903d0508c387ab69
psj <qkrtkdwns3410@naver.com>
2021-06-22 20:13:08 +0900

    6-22

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main() {
-//    int a = 5, b; //5
-//    printf("a=%d \n", a++);//a =5 ->이후 6
-//    b = ++a; //b=1+a //7
-//    printf("a= %d b =%d", a, b);//a=7 b=7
-//    printf("\n a= %d b =%d", a++, ++b); //a=a //7 //b=1+b/8
-//    int a = 5;
-//    int b = 2;
-//
-//    printf("%d / %d =%d", a, b, a / b);
-//    printf("%d / %d =%f", a, b, (double) a / b);
-    boolean op = 5 > 3 ? 1 : 0;
-    printf(op);
+    int a = 5;
+
+    if (a > 0) {
+        if (a >= 100) {
+            printf("Great");
+        } else {
+            printf("Good");
+        }
+    } else if (a < 0) {
+        printf("Negative");
+    } else {
+        printf("Zero");
+    }
+
+    return 0;
 }
