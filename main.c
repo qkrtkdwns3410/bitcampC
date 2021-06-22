ee6abbf16d712d6cf685f0fce79e73585fe77b31
psj <qkrtkdwns3410@naver.com>
2021-06-22 19:40:46 +0900

    6-22

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main() {
-
+//    int a = 5, b; //5
+//    printf("a=%d \n", a++);//a =5 ->이후 6
+//    b = ++a; //b=1+a //7
+//    printf("a= %d b =%d", a, b);//a=7 b=7
+//    printf("\n a= %d b =%d", a++, ++b); //a=a //7 //b=1+b/8
+//    int a = 5;
+//    int b = 2;
+//
+//    printf("%d / %d =%d", a, b, a / b);
+//    printf("%d / %d =%f", a, b, (double) a / b);
+    boolean op = 5 > 3 ? 1 : 0;
+    printf(op);
 }
