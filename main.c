c9f871b6daa0687e6573b5bd809c111c3327c1dc
psj <qkrtkdwns3410@naver.com>
2021-06-23 13:39:17 +0900

    6-22

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int i, j;
-
-    for (i = 2; i < 10; i++) {
-        printf("구구단 %d단 \n", i);
-        for (j = 1; j < 10; j++) {
-            printf("%d * %d  = %d\n", i, j, i * j);
+    int i=0, n;
+    int sum_odd = 0, sum_even = 0;
+    scanf("%d", &n); //입력한 정수 n
+    while (i <= n) {
+        if (i % 2 == 0) {
+            sum_even += i;
+        } else {
+            sum_odd += i;
         }
-        printf("\n\n");
+        i++;
     }
+    printf("짝수의 합: %d\n", sum_even);
+    printf("홀수의 합: %d\n", sum_odd);
+
 
     return 0;
+
 }
 
