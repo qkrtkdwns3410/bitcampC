872e19d0900d221fafd1fd28a9a05efff74673e9
psj <qkrtkdwns3410@naver.com>
2021-06-23 13:31:12 +0900

    6-22

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    printf("%d %d", a, b);
+    int i, j;
+
+    for (i = 2; i < 10; i++) {
+        printf("구구단 %d단 \n", i);
+        for (j = 1; j < 10; j++) {
+            printf("%d * %d  = %d\n", i, j, i * j);
+        }
+        printf("\n\n");
+    }
+
+    return 0;
 }
+
