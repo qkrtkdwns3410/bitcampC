897ea4307243d306fb51bf45d1435d8c1d3cc26e
psj <qkrtkdwns3410@naver.com>
2021-06-23 13:44:12 +0900

    6-22

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int i=0, n;
-    int sum_odd = 0, sum_even = 0;
-    scanf("%d", &n); //입력한 정수 n
-    while (i <= n) {
-        if (i % 2 == 0) {
-            sum_even += i;
-        } else {
-            sum_odd += i;
+    //0이 입력될 때까지 계속 수행되는 프로그램으로
+    //0이 아닌 정수값(n)에 대해 3으로 나머지 연산한
+    //결과를 출력하시오.
+    int n;
+    do {
+        scanf("%d", &n);
+        if (n != 0) {
+            printf("%d %% 3=%d\n", n, n % 3);
         }
-        i++;
-    }
-    printf("짝수의 합: %d\n", sum_even);
-    printf("홀수의 합: %d\n", sum_odd);
-
-
+    } while (n!=0);
     return 0;
 
 }
