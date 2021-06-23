aae5fa4cc208273b2fe077619f4efe1847bd19a8
psj <qkrtkdwns3410@naver.com>
2021-06-23 14:06:36 +0900

    6-22

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
+void add(int x, int y){
+    printf("%d+%d=%d", x, y, x + y);
+}
 int main() {
-    //0이 입력될 때까지 계속 수행되는 프로그램으로
-    //0이 아닌 정수값(n)에 대해 3으로 나머지 연산한
-    //결과를 출력하시오.
-    int n;
-    do {
-        scanf("%d", &n);
-        if (n != 0) {
-            printf("%d %% 3=%d\n", n, n % 3);
-        }
-    } while (n!=0);
+    int a, b;
+    scanf("%d %d", &a, &b);
+    add(a, b);
+
     return 0;
 
 }
+
+
 
