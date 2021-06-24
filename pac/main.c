84ec52b81459ba18114e1b53cadaeeaad200c81a
psj <qkrtkdwns3410@naver.com>
2021-06-24 15:27:22 +0900

    6-22

--- pac/main.c
+++ pac/main.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
+enum weekday{sun,mon,tue,wed,thu,fri, sat};
 
 int main() {
-    struct sungjuk {
-        char *name;
-        int kor, eng;
-        double avg;
-    };
-    struct sungjuk m2_1;
-    m2_1.name = "박상준";
-    m2_1.kor = 95;
-    m2_1.eng = 87;
+    enum weekday w_day;
+    printf("input 0 ~ 6: ");
+    scanf("%d", &w_day);
+    printf("\n");
+
+    switch (w_day) {
+        case sun:
+            printf("Sunday");
+            break;
+        case mon:
+            printf("Monday");
+            break;
+        case tue:
+            printf("Thueday");
+            break;
+        case wed:
+            printf("Wednesday");
+            break;
+        case thu:
+            printf("Thursday");
+            break;
+        case fri:
+            printf("F");
+            break;
+        case sat:
+            printf("sat");
+            break;
+        default:
+            printf("Input Correct Number");
+    }
+    return 0;
 }
 
 
