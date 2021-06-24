9eaae0b9d3b6af083442db7a2c1f2cf5fa020d30
psj <qkrtkdwns3410@naver.com>
2021-06-24 21:39:03 +0900

    6-22

--- pac/main.c
+++ pac/main.c
@@ -2,18 +2,18 @@
 #include <string.h>
 
 
-char* Reverse(const char *str) {
-    static char result[80]; //result 배열을 정적 지역 변수로 선언함
-    int i;
-    int len = strlen(str);
-    for (i = 0; i < len; i++) {
-        result[i] = str[len - i - 1];
+int main() {
+    int x[3] = {1, 2, 3};
+    int y[3] = {4, 5, 6};
+    int z[3] = {7, 8, 9};
+    int *arr[3] = {x, y, z};
+
+    int i, j;
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d", arr[i][j]);
+        }
+        printf("\n");
     }
-    result[i] = '\0';
-
-    return result;
-}
-
-int main(){
-    return Reverse();
 }
